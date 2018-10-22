bb0ff1d054f661602eba2195fb0257f4dd4b226e
Sam Wolfe <wolfe.766@osu.edu>
2018-10-22 10:27:51 -0400

    no changes

--- app/src/main/java/aclass/champion/android/streetart/MainActivity.java
+++ app/src/main/java/aclass/champion/android/streetart/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "Main Activity";
